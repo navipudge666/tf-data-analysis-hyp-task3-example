97bc63c9bda79726f702c779d173e71939ae3599
navipudge666 <33724392+navipudge666@users.noreply.github.com>
2023-04-13 23:54:11 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 475225606 # Ваш chat ID, не меняйте название переменной
 
 def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04
+    t_statistic, p_value = st.ttest_1samp(x, 500, alternative="two-sided")
+    return p_value < 2*alpha and x.mean() > 500
