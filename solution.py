4041bd68dd8929c87a61bfe27eb4919eb3c38798
navipudge666 <33724392+navipudge666@users.noreply.github.com>
2023-04-14 00:02:01 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ import scipy.stats as st
 
 chat_id = 475225606 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.04
     t_statistic, p_value = st.ttest_1samp(x, 500, alternative="two-sided")
     return p_value < 2*alpha and x.mean() > 500
